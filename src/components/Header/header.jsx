9a82367d2d8788634d13e8539ac8be2def3cfcac
gunysunny <mikan31@naver.com>
2025-05-30 17:35:34 +0900

    이미지 임포트방식을 줄이기 위해 public폴더방식으로 진행 (_이미지폴더 이동동_)

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,34 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './header.css'; // 같은 폴더니까 유지
-
-// 이미지 경로는 상위로 올라가서 images 폴더로 접근해야 함
-import cartImg from '../../images/top_right_menu2.png';
-import logoImg from '../../images/Logo.png';
-import heartImg from '../../images/top_right_menu1.png';
-import userImg from '../../images/top_right_menu3.png';
-import searchIcon from '../../images/search.png';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './header.css'
 
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="logo">
-                <Link to="/" onClick={() => console.log('로고 클릭됨')}>
-                    <img src={logoImg} alt="logo" className="logo" />
-                </Link>
-            </div>
-            <div className="search-container">
-                <img src={searchIcon} alt="search" className="search-icon" />
-                <input type="text" placeholder="Search..." className="search-input" />
-            </div>
-            <div className="spacer" /> {/* ← 가상의 여백 영역 */}
-            <div className="icons">
-                <img src={cartImg} alt="cart" className="icon-img" />
-                <img src={heartImg} alt="heart" className="icon-img" />
-                <img src={userImg} alt="user" className="icon-img" />
-            </div>
-        </header>
-    );
-};
+  // 배포 시 서브디렉터리에 올라가더라도 PUBLIC_URL이 자동으로 맞춰집니다
+    const PUBLIC_URL = process.env.PUBLIC_URL
 
-export default Header;
+    return (
+        <header className="flex max-w-[1440px] justify-between items-center px-8 py-4 mx-auto">
+        {/* 로고 */}
+        <div className="cursor-pointer w-[120px] flex-shrink-0">
+            <Link to="/" onClick={() => console.log('로고 클릭됨')}>
+            <img
+                src={`${PUBLIC_URL}/images/Logo.png`}
+                alt="logo"
+                className="w-auto h-10"
+            />
+            </Link>
+        </div>
+
+        {/* 검색창 */}
+        <div className="flex-[2] flex items-center bg-[#ececec] rounded px-4 py-2 max-w-[300px] w-full mx-5">
+            <img
+            src={`${PUBLIC_URL}/images/search.png`}
+            alt="search"
+            className="w-[18px] h-[18px] mr-2"
+            />
+            <input
+            type="text"
+            placeholder="Search..."
+            className="flex-1 bg-transparent border-none outline-none text-sm"
+            />
+        </div>
+
+        {/* 아이콘들 */}
+        <div className="w-[120px] flex gap-5 items-center">
+            <img
+            src={`${PUBLIC_URL}/images/top_right_menu2.png`}
+            alt="cart"
+            className="w-6 h-6 cursor-pointer"
+            />
+            <img
+            src={`${PUBLIC_URL}/images/top_right_menu1.png`}
+            alt="heart"
+            className="w-6 h-6 cursor-pointer"
+            />
+            <img
+            src={`${PUBLIC_URL}/images/top_right_menu3.png`}
+            alt="user"
+            className="w-6 h-6 cursor-pointer"
+            />
+        </div>
+        </header>
+    )
+}
+
+export default Header
