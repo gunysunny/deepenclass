f9b366dc78f25af8e9914519c5837b8f1939a2e0
gunysunny <mikan31@naver.com>
2025-06-05 15:36:01 +0900

    코드 정리 evn삭제제

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './header.css'
-
-
+import ShowNow from '@/components/Assets/shownow_Btn'
 
 const ICONS = [
     { name: 'cart', file: 'top_right_menu2.png' },
@@ -10,53 +8,59 @@ const ICONS = [
     { name: 'user', file: 'top_right_menu3.png' },
 ]
 
-
-// 이미지 경로를 합쳐 주는 용도
-const baseImg = process.env.PUBLIC_URL + '/images'
-
-
-export default function Header(){
+    export default function Header() {
     return (
         <header className="flex max-w-[1440px] justify-between items-center px-8 py-4 mx-auto">
-        {/* 1) 로고 영역을 작은 컴포넌트로 분리 */}
-        <LogoLink />
+            {/* 1) 로고 영역을 작은 컴포넌트로 분리 */}
+            <LogoLink />
 
-        {/* 2) 검색창 */}
-        <SearchBar baseImg={baseImg} />
+            {/* 2) 검색창 */}
+            <SearchBar />
 
-        {/* 3) 아이콘들: 배열 + map 으로 간단하게 */}
-        <div className="w-[120px] flex gap-5 items-center">
-            {ICONS.map(({ name, file }) => (
-            <img key={name} src={`${baseImg}/${file}`} alt={name} className="w-6 h-6 cursor-pointer" />
-            ))}
-        </div>
-    </header>
+            {/* 3) 아이콘들: 배열 + map 으로 간단하게 */}
+            <div className="w-[120px] flex gap-5 items-center">
+                {ICONS.map(({ name, file }) => (
+                <img
+                    key={name}
+                    src={`/images/${file}`}
+                    alt={name}
+                    className="w-6 h-6 cursor-pointer"
+                />
+                ))}
+            </div>
+        </header>
     )
-}
+    }
 
-
-
-
-
-// LogoLink 컴포넌트
-function LogoLink() {
-    const baseImg = process.env.PUBLIC_URL + '/images'
+    // LogoLink 컴포넌트
+    function LogoLink() {
     return (
-        <div className='cursor-pointer w-[120px] flex-shrink-0'>
-            <Link to="/" onClick={() => console.log("로고 클릭")}>
-                <img src={`${baseImg}/Logo.png`} alt="logo" className='w-auto h-10' />
+        <div className="cursor-pointer w-[120px] flex-shrink-0">
+            <Link to="/" onClick={() => console.log('로고 클릭')}>
+                <img
+                src={`/images/Logo.png`}
+                alt="logo"
+                className="w-auto h-10"
+                />
             </Link>
         </div>
     )
-}
+    }
 
-
-// SearchBar 컴포넌트
-function SearchBar({ baseImg }) {
+    // SearchBar 컴포넌트
+    function SearchBar() {
     return (
         <div className="flex-[2] flex items-center bg-[#ececec] rounded px-4 py-2 max-w-[300px] w-full mx-5">
-            <img src={`${baseImg}/search.png`} alt="search" className="w-[18px] h-[18px] mr-2" />
-            <input type="text" placeholder="Search..." className="flex-1 bg-transparent border-none outline-none text-sm" />
+            <img
+                src={`/images/search.png`}
+                alt="search"
+                className="w-[18px] h-[18px] mr-2"
+            />
+            <input
+                type="text"
+                placeholder="Search..."
+                className="flex-1 bg-transparent border-none outline-none text-sm"
+            />
         </div>
     )
 }
