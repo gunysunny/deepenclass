3208957bdfaab75c8793865975a0882f78b539d9
gunysunny <mikan31@naver.com>
2025-06-05 21:10:27 +0900

    폴더이름 변경

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import React from 'react';
-import Header from './components/Header/Header.jsx';
-import Banner from './components/Section/Banner.jsx';
-import Contents from './components/Contents/Contents.jsx';
-import Category from './components/Section/Category.jsx';
-import ProductList from './components/Section/ProductList.jsx';
+import Header from '@/components/Header/Header.jsx';
+import Banner from '@/components/Section/Banner.jsx';
+import Contents from '@/components/Contents/Contents.jsx';
+import Category from '@/components/Section/Category.jsx';
+import ProductList from '@/components/Section/ProductList.jsx';
 
 
 function App() {
